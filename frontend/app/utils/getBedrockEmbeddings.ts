9adf645fc41af1b5c17e8fef065ec4b7c3e4ecac
dermot <dermot@azafinance.com>
2024-08-29 15:38:24 +0100

    updated the initial questions to be aza specific

--- frontend/app/utils/getBedrockEmbeddings.ts
+++ frontend/app/utils/getBedrockEmbeddings.ts
@@ -2,7 +2,7 @@ import "server-only";
 
 import { BedrockEmbeddings } from "@langchain/aws";
 import { BedrockRuntimeClient } from "@aws-sdk/client-bedrock-runtime";
-import { fromNodeProviderChain } from "@aws-sdk/credential-providers";
+import { fromNodeProviderChain, fromEnv } from "@aws-sdk/credential-providers";
 
 export async function getBedrockEmbeddings() {
   const credentials = fromNodeProviderChain({
@@ -11,7 +11,7 @@ export async function getBedrockEmbeddings() {
 
   const client = new BedrockRuntimeClient({
     region: "eu-west-2",
-    credentials: credentials,
+    credentials: fromEnv(),
   });
 
   return new BedrockEmbeddings({
